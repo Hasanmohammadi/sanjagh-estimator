d60661f17c6fb636424b1d8627c0df01c76a854b
Hassan Mohammadi <hassanshm97@gmail.com>
2026-06-17 12:21:17 +0330

    add: icons and accordion CP

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,9 @@
 import BackRightIcon from "./BackRightIcon";
 import SettingsIcon from "./SettingsIcon";
 import ReceiptIcon from "./ReceiptIcon";
+import ChevronUpIcon from "./ChevronUpIcon";
+import ChevronDownIcon from "./ChevronDownIcon";
+import DownloadIcon from "./DownloadIcon";
+import EditIcon from "./EditIcon";
 
-export { BackRightIcon, SettingsIcon, ReceiptIcon };
+export { BackRightIcon, SettingsIcon, ReceiptIcon, ChevronUpIcon, ChevronDownIcon, DownloadIcon, EditIcon };
